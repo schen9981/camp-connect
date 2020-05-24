9edebba6100c0c2b68484c0a28cd7631888baaab
Sophia <schen9981@gmail.com>
2020-05-24 14:54:36 -0600

    about page skeleton

---
+++ camp-connect/src/pages/About/About.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./About.css";
+
+type AboutPageProps = {
+};
+
+type AboutPageState = {
+};
+
+export default class About extends React.Component<AboutPageProps, AboutPageState> {
+  constructor(props: AboutPageProps) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render () {
+    return (
+      <div className="About">
+
+      </div>
+    )
+  }
+}

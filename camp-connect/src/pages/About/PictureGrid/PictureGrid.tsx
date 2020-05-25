0778d4d632f0ca62729e93396892031415cfd733
hollyyuqizheng <holly_zyq@126.com>
2020-05-24 22:22:44 -0400

    editing the grid

--- camp-connect/src/pages/About/PictureGrid/PictureGrid.tsx
+++ camp-connect/src/pages/About/PictureGrid/PictureGrid.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./PictureGrid.css";
-
 //import bio from "bio.json";
 
 type Person = {
@@ -25,19 +24,19 @@ export default class PictureGrid extends React.Component<PictureGridProps, Pictu
 
       people: [
         {
-            "name": "Alex Morishige",
-            "imagePath": "../../../assets/headshots/alex-morishige.png",
-            "bio": "hi"
+            name: "Alex Morishige",
+            imagePath: "../../../assets/headshots/alex_morishige.png",
+            bio: "hi"
         },
         {
-            "name": "Callie Teitelbaum",
-            "imagePath": "../../../assets/headshots/callie_teitelbaum.png",
-            "bio": "hi"
+            name: "Callie Teitelbaum",
+            imagePath: "../../../assets/headshots/callie_teitelbaum.png",
+            bio: "hi"
         },
         {
-            "name": "Gabby Asuncion",
-            "imagePath": "../../../assets/headshots/gabby_asuncion.png",
-            "bio": "hi"
+            name: "Gabby Asuncion",
+            imagePath: "../../../assets/headshots/gabby_asuncion.png",
+            bio: "hi"
         }
 
       ],
@@ -54,20 +53,58 @@ export default class PictureGrid extends React.Component<PictureGridProps, Pictu
 
   render () {
     return (
-      <div className="PictureGrid">
-        <div className="pictures" style={{display: 'grid', gridTemplateColumns: this.props.cols, gridTemplateRows: this.props.rows}}>
-          
-          {this.state.people.map((person) => {
-            <div className="person" onClick={() => this.setCurrentDisplay(person.name)}>
-              <img src={person.imagePath}></img>
-              <p> {person.name} </p>
+      <div className="pictureGrid">
+        
+        {/*  style={{gridTemplateColumns: this.props.cols, gridTemplateRows: this.props.rows}}   */}
 
+        <div className="pictures">
+          <div className="smallGrid">
+            <img src="../../../assets/headshots/alex_morishige.png"/>
+            <p> Alex </p>
+            <p> Bio </p>
+          </div>
+
+          <div className="smallGrid">
+            <img src="../../../assets/headshots/alex_morishige.png"/>
+            <p> Alex </p>
+            <p> Bio </p>
+          </div>
+
+          <div className="smallGrid">
+            <img src="../../../assets/headshots/alex_morishige.png"/>
+            <p> Alex </p>
+            <p> Bio </p>
+          </div>
+
+          <div className="smallGrid">
+            <img src="../../../assets/headshots/alex_morishige.png"/>
+            <p> Alex </p>
+            <p> Bio </p>
+          </div>
+
+          <div className="smallGrid">
+            <img src="../../../assets/headshots/alex_morishige.png"/>
+            <p> Alex </p>
+            <p> Bio </p>
+          </div>
+
+
+
+          {this.state.people.map((person) => {
+            console.log(person.name); 
+
+            <div className="smallGrid" onClick={() => this.setCurrentDisplay(person.name)}>
+              
+              <img src={person.imagePath} alt={person.name} />
+              <p> {person.name} </p>
               <span className={this.state.currentDisplay == person.name ? "bioShow" : "bio"}>
                 <p>{person.name}</p>
                 <p>{person.bio}</p>
               </span>
 
             </div>
+
+            console.log("here");
           })}
 
         </div>

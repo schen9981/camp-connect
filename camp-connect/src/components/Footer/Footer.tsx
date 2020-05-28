d0777cee42e434f941f1e3077947d309bdf896ba
Gabby Asuncion <gabrielaasuncion@gmail.com>
2020-05-27 17:58:35 -0700

    update a bunch of stuff

--- camp-connect/src/components/Footer/Footer.tsx
+++ camp-connect/src/components/Footer/Footer.tsx
@@ -1,10 +1,10 @@
 import Nav from "react-bootstrap/Nav";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
-import React from 'react';
-import './Footer.css';
-import ig from '../../assets/ig-logo.png';
-import mail from '../../assets/mail-512.png';
+import React from "react";
+import "./Footer.css";
+import ig from "../../assets/ig-logo.png";
+import mail from "../../assets/mail-512.png";
 
 type FooterProps = {};
 
@@ -20,22 +20,34 @@ export default class Footer extends React.Component<FooterProps, FooterState> {
     return (
       <div className="rect">
         <div className="company-info">
-          <p>Camp Connect <br />
-            New York, NY</p>
+          <p>
+            Camp Connect <br />
+            New York, NY
+          </p>
           <p>2020 Camp Connect</p>
         </div>
 
-        <Navbar className="navbar" expand="lg">
+        <Navbar className="navbar-dark" expand="lg">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" >
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end footer-nav-links"
+          >
             <Nav className="nav">
               <Nav.Link href="/about">Our Team</Nav.Link>
               <Nav.Link href="/schedule">A Week @ Camp</Nav.Link>
-              <Nav.Link href="/ig"><img src={ig} />@campconnect</Nav.Link>
-              <Nav.Link href="/email"><img src={mail} />campconnect@gmail.com</Nav.Link>
+              <Nav.Link href="/ig">
+                <img src={ig} />
+                @campconnect
+              </Nav.Link>
+              <Nav.Link href="/email">
+                <img src={mail} />
+                campconnect@gmail.com
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-      </div >);
+      </div>
+    );
   }
 }
